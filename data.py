e50ebb6a1d5929d037dbd3bf5d4f5aafec947fb4
Ali Gholami <gholami@sfu.ca>
2019-10-26 16:42:38 -0700

    Update: Separate train/val samples

--- data.py
+++ data.py
@@ -3,8 +3,31 @@ from torch.utils.data.dataset import Dataset
 
 
 class ANetCaptionsDataset(Dataset):
-    def __init__(self, anet_json_path):
+    def __init__(self, anet_json_path, train=True):
         self.anet_contents = self.read_json_file(anet_json_path)
+        self.anet_subset = {}
+
+        def get_subset(content_dict, subset):
+            """
+            Return a subset of the dataset (train or validation) based on the subset type.
+            :param content_dict: Raw dataset dictionary.
+            :param subset: String either 'training' or 'validation'.
+            :return: A dictionary containing the desired subset.
+            """
+            subset_dict = {}
+            if subset == 'training':
+                subset_dict = {key: value for key, value in content_dict['database'].items() if
+                               value['subset'] == 'training'}
+            else:
+                subset_dict = {key: value for key, value in content_dict['database'].items() if
+                               value['subset'] == 'validation'}
+
+            return subset_dict
+
+        if train == True:
+            self.anet_subset = get_subset(self.anet_contents, 'training')
+        else:
+            self.anet_subset = get_subset(self.anet_contents, 'validation')
 
     def __len__(self):
         pass
@@ -22,5 +45,8 @@ class ANetCaptionsDataset(Dataset):
             return json.load(fo)
 
 
-anet_json_path = '../gvd-data/ActivityNet/data/anet/anet_annotations_trainval.json'
-anet_captions = ANetCaptionsDataset(anet_json_path)
+anet_path = '../gvd-data/ActivityNet/data/anet/anet_annotations_trainval.json'
+train_anet_captions = ANetCaptionsDataset(anet_path, train=True)
+validation_anet_captions = ANetCaptionsDataset(anet_path, train=False)
+print(len(train_anet_captions.anet_subset))
+print(len(validation_anet_captions.anet_subset))
